c70f20ccf593cec3170b44db2325d6e214130642
Hakan Hidir <hakan.hidir@technicsatelier.com>
2022-11-19 02:12:13 +0100

    init

---
+++ tarfiles-support/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Formats.Tar;
+
+
+public static class Program
+{
+
+    const string SOURCE = "../../../Source";
+    const string TARFILEPATH = "../../../Destination/sample.tar";
+    const string DECOMPRESSFOLDER = "../../../DecompressSource";
+
+    public async static Task Main(string[] args)
+    {
+        await CompressTar();
+        await DeCompressTar();
+    }
+
+    public static async Task CompressTar()
+    {
+        if (!File.Exists(TARFILEPATH))
+        {
+            await TarFile.CreateFromDirectoryAsync(SOURCE, TARFILEPATH, false, default).ConfigureAwait(false);
+            Console.WriteLine("compressed succcessfully.");
+        }
+
+    }
+
+    public static async Task DeCompressTar()
+    {
+        await TarFile.ExtractToDirectoryAsync(TARFILEPATH, DECOMPRESSFOLDER, false, default).ConfigureAwait(false);
+        Console.WriteLine("decompressed succcessfully.");
+    }
+}
+
+
